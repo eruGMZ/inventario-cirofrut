61bdfc4afa228d4e2b80346bf8fdb9251a9a6082
eruGMZ <erick20eru@gmail.com>
2025-05-02 20:54:16 -0600

    Login-breeze Install

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,21 @@
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body>
-    <h1>Bienvenido al sistema Citrofrut</h1>
+<body class="bg-gray-50 text-center flex flex-col items-center justify-center min-h-screen">
+
+    <h1 class="text-3xl font-bold text-blue-900 mb-8">Bienvenido al sistema Citrofrut</h1>
+
+    <div class="flex space-x-4">
+        <a href="{{ route('login') }}"
+           class="px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition">
+            Iniciar Sesión
+        </a>
+
+        <a href="{{ route('register') }}"
+           class="px-6 py-2 bg-green-600 text-white rounded hover:bg-green-700 transition">
+            Registrarse
+        </a>
+    </div>
+
 </body>
 </html>
